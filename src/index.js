9224a10fa7d431ff421b17e7f554537a58854954
chaikuna <chaikuna@scg.com>
2022-06-29 14:10:09 +0700

    feat: health check

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/health_check", (req, res) => {
+  res.json({ status: "ok", version: require("../package.json")["version"] });
 });
 
 app.listen(port, () => {
